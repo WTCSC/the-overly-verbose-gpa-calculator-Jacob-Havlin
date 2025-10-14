ce2284c3647945fbb6428bfca5d03f56c5f5d70c
2183746 <jacobhavlin91@gmail.com>
2025-10-14 14:46:05 -0600

    git commit

---
+++ GPA.py
@@ -0,0 +1,3 @@
+print("Welcome to the one and only GPA Calculator that talks to much!")
+print("I am going to calculate your GPA based on the 4.0 scale and how many classes you have taken.")
+classes = input("")
