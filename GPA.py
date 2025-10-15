285d45b29adb7ac0398a61b98f8934f5c90b5a9b
2183746 <jacobhavlin91@gmail.com>
2025-10-15 12:22:31 -0600

    git commit

--- GPA.py
+++ GPA.py
@@ -1,3 +1,6 @@
 print("Welcome to the one and only GPA Calculator that talks to much!")
 print("I am going to calculate your GPA based on the 4.0 scale and how many classes you have taken.")
-classes = input("")
+subjects = int(input("How many classes would you like to calculate? "))
+for subject in range(subjects):
+    sentence = float(input(f"Please enter your grade (0.0-4.0) for class {subject+1} "))
+    
