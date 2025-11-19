91ab3434731a689ce28d8289a244359da566c7dc
Jacob <jacobhavlin91@gmail.com>
2025-11-18 19:57:19 -0700

    git commit

--- GPA_revised.py
+++ GPA_revised.py
@@ -1,11 +1,11 @@
-p=print
+p=print;I=input
 p("Welcome to the one and only GPA Calculator that talks to much!\nI am going to calculate your GPA based on the 5.0 scale and how many classes you have taken.")
 l=[]
-t=int(input("How many classes would you like to calculate? "))
+t=int(I("How many classes would you like to calculate? "))
 for i in range(t):
     while True:
         try:
-            g=float(input(f"Please enter your grade (0.0-5.0) for class {i+1} "))
+            g=float(I(f"Please enter your grade (0.0-5.0) for class {i+1} "))
             if 0<=g<=5:l.append(g);break
             p("Your grade must be in between 0.0 and 5.0")
         except:p("Please enter a numerical value!")
@@ -13,15 +13,15 @@ a=round(sum(l)/len(l),2)
 p(f"Calculating GPA...\nyour overall GPA is currently at a {a}")
 while True:
     try:
-        s=int(input("Which semester would you like to focus on? "))
+        s=int(I("Which semester would you like to focus on? "))
         if s in[1,2]:r=["1st","2nd"][s-1];break
         p("You only have 2 semesters silly!")
     except:p("In case you didn't know a semester is expressed in the form of either 1 or 2.")
-while 1:
+while True:
  try:
   c=int(I(f"How many classes were in the {r} semester? "))
-  if 0<c<=len(L):break
-  p("You must have taken at least 1 class right!")if c<=0 else p(f"You only entered {len(L)} classes total!")
+  if 0<c<=len(l):break
+  p("You must have taken at least 1 class right!")if c<=0 else p(f"You only entered {len(l)} classes total!")
  except:p("Please enter a valid number.")
 sem=l[:c]if s==1 else l[t-c:]if t-c>=0 else[]
 if sem:
@@ -34,11 +34,11 @@ else:p(f"There was an error calculating your {r} semester GPA due to incorrect i
 p("You are going to set a new overall GPA goal")
 while True:
     try:
-        goal=float(input("What is your goal GPA? "))
+        goal=float(I("What is your goal GPA? "))
         if 0<=goal<=5:
             if a>=goal:
                 p("Congratulations you have already met this goal")
-                if input("Would you like to set a different goal? (yes/no) ").lower()=="no":p("Keep up the good work");exit()
+                if I("Would you like to set a different goal? (yes/no) ").lower()=="no":p("Keep up the good work");exit()
             else:break
         p("Your goal GPA needs to be in between 0.0 and 5.0")
     except:p("Your GPA must be a valid number")
